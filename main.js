bfbc3a182a71acaf7f2c37b9977991687567cc50
Ashank586 <80754565+Ashank586@users.noreply.github.com>
2021-06-14 17:37:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name="Ashank";
+var second_name="Vatsa";
+var first_number="3";
+var second_number="69";
+var full_name=first_name.concat(second_name);
+var sum=first_number+second_number;
+console.log(full_name);
+console.log(first_name+""+second_name);
+console.log(sum);
